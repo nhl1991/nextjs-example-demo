e6bd6f09e0c8326ff829b7d0af30ba77518b035f
Laslark <laslark1991@gmail.com>
2025-08-29 23:49:48 +0900

    Add link to CSS-study

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ export default function Home() {
   
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <div className="flex flex-wrap p-4 gap-2">
+      <div className="max-w-[48rem] flex flex-wrap p-4 gap-2">
         <LinkContainer link="https://position-playground.vercel.app" />
+
+        <LinkContainer link="/https://css-study-nine.vercel.app/" />
         <LinkContainer link="/device-detection-demo" />
         <LinkContainer link="/copy-paste" />
         <LinkContainer link="/hooks" />
         <LinkContainer link="/components" />
         <LinkContainer link="/file-convention" />
         <LinkContainer link="/blackhole" />
+        
       </div>
     </div>
   );
