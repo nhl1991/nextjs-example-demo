1521bfccbc425770b02e5a3bb01787e091a25a2b
Laslark <laslark1991@gmail.com>
2025-12-01 20:46:39 +0900

    CSS fix

--- src/components/ui/LinkContainer.tsx
+++ src/components/ui/LinkContainer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function LinkContainer({ link } : { link : string}){
 
   return(
-    <div className="relative rounded-xl p-1 bg-black dark:bg-white hover:bg-gradient-to-r hover:from-pink-500 hover:to-blue-500">
+    <div className="relative rounded-xl p-1 bg-white dark:bg-black hover:bg-gradient-to-r hover:from-pink-500 hover:to-blue-500">
         <Link className="bg-white rounded-lg px-4 py-1" href={link}>{link}</Link>
         
 
