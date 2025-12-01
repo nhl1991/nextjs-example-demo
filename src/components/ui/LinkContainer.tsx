e55141b336ccceefb44aae7d4738cb544eb25bc2
Laslark <laslark1991@gmail.com>
2025-12-02 00:21:48 +0900

    chore: css fix

--- src/components/ui/LinkContainer.tsx
+++ src/components/ui/LinkContainer.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 
-export default function LinkContainer({ link } : { link : string}){
-
-  return(
-    <div className="relative rounded-xl p-1 bg-white dark:bg-black hover:bg-gradient-to-r hover:from-pink-500 hover:to-blue-500">
-        <Link className="bg-white rounded-lg px-4 py-1" href={link}>{link}</Link>
-        
-
-</div>
-  )
+export default function LinkContainer({ link }: { link: string }) {
+  return (
+    <div className="rounded-xl relative">
+      
+      <div className="relative p-1 rounded-xl border-gradient">
+        <Link
+          className="bg-white dark:bg-black rounded-lg px-4 py-1"
+          href={link}
+        >
+          {link}
+        </Link>
+      </div>
+    </div>
+  );
 }
