2ea241887c92644282a77e110afd44e72b3053dc
Laslark <laslark1991@gmail.com>
2026-01-05 20:11:01 +0900

    fix: force dynamic rendering

--- src/app/parellel-routes/@A/page.tsx
+++ src/app/parellel-routes/@A/page.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 
 export default async function Slot() {
   await new Promise((r) => setTimeout(r, 5000));
--- src/app/parellel-routes/@B/page.tsx
+++ src/app/parellel-routes/@B/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function Slot() {
   await new Promise((r) => setTimeout(r, 3000));
   return <div className="w-90 h-48 rounded-xl border-2 border-green-200 shadow-xl flex items-center justify-center">
