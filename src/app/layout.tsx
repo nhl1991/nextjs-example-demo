9bc30a52c161071d3736a28d8a63ac908d40951f
Laslark <laslark1991@gmail.com>
2026-01-05 20:44:46 +0900

    fix: character escape

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import "@radix-ui/themes/styles.css";
 import Navigation from "../components/common/navigation";
 
 const geistSans = Geist({
